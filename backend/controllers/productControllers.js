8e5109730f910ee28067e501981c71d02f311ac5
prajjwal98dubey <prajjwaldubey003@gmail.com>
2024-08-08 23:37:40 +0530

    basic implementation with the help of req headers user-agent property.

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -3,7 +3,6 @@ const Product = require('../models/ProductModal')
 const getAllProducts = async (req, res) => {
     // web needs all info whereas mobile needs "name,price,brand,image"
     let user = req.user
-    // mobile user-agent => "Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36"
     try {
         if (user === 'web') {
             const allProducts = await Product.find()
@@ -22,3 +21,8 @@ const getAllProducts = async (req, res) => {
 
 
 module.exports = { getAllProducts }
+
+
+// mobile (Andorid) user-agent => "Mozilla/5.0 (Linux; Android 10; Pixel 3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Mobile Safari/537.36"
+
+// mobile (ios) user-agent => "Mozilla/5.0 (iPhone; CPU iPhone OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15E148 Safari/604.1" 
